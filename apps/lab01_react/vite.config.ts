852f0d395ac0df88a7716ef2eb3fce9973f5d198
saw4nya <sawanya.si@kkumail.com>
2025-11-24 17:45:23 +0700

    set base path for lab01_react and lab01_vue configurations

--- apps/lab01_react/vite.config.ts
+++ apps/lab01_react/vite.config.ts
@@ -8,6 +8,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './mobileweb/lab01_react/',
   plugins: [
     react(),
     legacy()
--- apps/lab01_vue/vite.config.ts
+++ apps/lab01_vue/vite.config.ts
@@ -7,6 +7,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: './mobileweb/lab01_vue/',
   plugins: [
     vue(),
     legacy()
