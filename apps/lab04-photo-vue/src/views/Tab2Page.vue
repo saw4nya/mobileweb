8d214c247ed6475605606d70ac8a8eb5ed0a315a
saw4nya <sawanya.si@kkumail.com>
2026-01-11 18:11:28 +0700

    Photo Gallery

---
+++ apps/lab04-photo-vue/src/views/Tab2Page.vue
@@ -0,0 +1,34 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Photo Gallery</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Photo Gallery</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      <ion-fab vertical="bottom" horizontal="center" slot="fixed">
+        <!-- CHANGE: Add a click event listener to the floating action button. -->
+        <ion-fab-button @click="addNewToGallery()">
+          <ion-icon :icon="camera"></ion-icon>
+        </ion-fab-button>
+      </ion-fab>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { camera } from 'ionicons/icons';
+import { IonPage, IonHeader, IonFab, IonFabButton, IonIcon, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+
+// CHANGE: Add `usePhotoGallery` import
+import { usePhotoGallery } from '@/composables/usePhotoGallery';
+
+// CHANGE: Destructure `addNewToGallery` from `usePhotoGallery()
+const { addNewToGallery } = usePhotoGallery();
+</script>
